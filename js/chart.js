d2cdda795d8f79c0593fce584cb3f9a72e4066ce
theo-oriol <45877614+theo-oriol@users.noreply.github.com>
2020-12-01 22:48:21 +0100

    V2

--- js/chart.js
+++ js/chart.js
@@ -1,20 +1,24 @@
-var ctx = document.getElementById('myChart').getContext('2d');
+//var ctx = document.getElementById('myChart').getContext('2d');
 
 function chart_city(json,city){
+
+    let div = document.getElementById("title");
+    let para = document.createElement("P");               // Create a <p> element
+    para.innerText = "city";                        // Insert text
+    div.appendChild(para);                      // Append <p> to <body>
+
+    ctx = create_ctx(1) // create canvas for each graph and return context in an array  
     data = {
-        'labels': ["death","hospitalizedCurrently"],
+        'labels': ["death","hospitalizedCurrently","positive"],
         'datasets':[{
-            'data':[json["death"],json["hospitalizedCurrently"]],
-            "backgroundColor": ["#3e95cd", "#8e5ea2"]
+            'data':[json["death"],json["hospitalizedCurrently"],json["positive"]],
+            'backgroundColor': ["#3E2F2F", "#B32525",'#E39C9C']
         }],
     }
     console.log("data 2",data);
     hbar(data,city+" Population : "+json["population"])
 }
 
-function chart_state(json){
-    
-}
 
 function hbar(data,text){
     console.log("data",data);
@@ -22,12 +26,12 @@ function hbar(data,text){
         type: 'horizontalBar',
         data: data,
         options: {
-            "title":{
-                "text":text,
-                "display": true,
+            'title':{
+                'text':text,
+                'display': true,
             },
-            "legend": { 
-                "display": false,
+            'legend': { 
+                'display': false,
              },
 
         }
